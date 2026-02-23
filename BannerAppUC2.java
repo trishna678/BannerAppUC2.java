d1e214b1a6ea23de70dd3ec296b481c7cc8f3cdd
trishna g <tg0839@srmist.edu.in>
2026-02-23 03:14:37 +0000

    Added UC2 banner implementation

---
+++ BannerAppUC2.java
@@ -0,0 +1,14 @@
+public class BannerAppUC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****      *****      ******      *****   ");
+        System.out.println(" *     *    *     *     *     *    *     *  ");
+        System.out.println(" *     *    *     *     *     *    *        ");
+        System.out.println(" *     *    *     *     ******      *****   ");
+        System.out.println(" *     *    *     *     *                *  ");
+        System.out.println(" *     *    *     *     *          *     *  ");
+        System.out.println("  *****      *****      *           *****   ");
+
+    }
+}
